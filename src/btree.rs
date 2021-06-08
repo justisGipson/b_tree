9aeba4ede8ef84241457159bd6b4f806b2930401
Justis <justis710@gmail.com>
2021-06-08 14:01:43 -0400

    btree functions

--- src/btree.rs
+++ src/btree.rs
@@ -97,5 +97,119 @@ impl BTree {
   /// insert a key value pair possibly splitting nodes along the way
   pub fn insert(&mut self, kv: KeyValuePair) -> Result<(), Error> {
     let root_page = self.pager.get_page(&self.root_offset)?;
+    let mut root = Node::try_from(root_page)?;
+    if self.is_node_full(&root)? {
+      let mut old_root = &mut root;
+      let old_root_offset = self.root_offset.clone();
+      let mut new_root = Node::new(NodeType::Internal(vec![], vec![]), true, None);
+      // write the new root to disk
+      let new_root_offset = self.pager.write_page(Page::try_from(&new_root)?)?;
+      // Set the current roots parent to the new root
+      old_root.parent_offset = Some(new_root_offset.clone());
+      old_root.is_root = false;
+      // update root offset.
+      self.root_offset = new_root_offset;
+      // split old_root
+      let (median, sibling) = old_root.split(self.b)?;
+      // write olf root with tts new data to disks
+      self.pager.write_page_at_offset(Page::try_from(&*old_root)?, &old_root_offset)?;
+      // write the newly created sibling with its children and key
+      let sibling_offset = self.pager.write_page(Page::try_from(&sibling)?)?;
+      // update new root with its children and key
+      new_root.node_type = NodeType::Internal(vec![old_root_offset, sibling_offset], vec![median]);
+      // write the new_root to disk
+      self.pager.write_page_at_offset(Page::try_from(&new_root), &self.root_offset)?;
+      // assign new root
+      root = new_root;
+    }
+    self.insert_non_full(&mut root, self.root_offset.clone(), kv)
+  }
+
+  /// insert_non_full (recursively) finds a node rooted at a fiven non-full node
+  /// to insert a given kv pair
+  fn insert_non_full(
+    &mut self,
+    node: &mut Node,
+    node_offset: Offset,
+    kv: KeyValuePair,
+  ) -> Result<(), Error> {
+    match &mut node.node_type {
+      NodeType::Leaf(ref mut pairs) => {
+        let idx = pairs.binary_search(&kv).unwrap_or_else(|x| x);
+        pairs.insert(idx, kv);
+        self.pager.write_page_at_offset(Page::try_from(&*node)?,&node_offset)
+      }
+      NodeType::Internal(ref mut children, ref mut keys) => {
+        let idx = keys.binary_search(&Key(kv.key.clone())).unwrap_or_else(|x| x);
+        let child_offset = children.get(idx).ok_or(Error::UnexpectedError)?.clone();
+        let child_page = self.pager.get_page(&child_offset)?;
+        let mut child = Node::try_from(child_page)?;
+        if self.is_node_full(&child)? {
+          // split will split the child at b leaving the [0, b-1] keys
+          // while moving the set of [b, 2b-1] keys to the sibling
+          let (median, mut sibling) = child.split(self.b)?;
+          self.pager.write_page_at_offset(Page::try_from(&child)?, &child_offset)?;
+          // write newly created sibling to disk
+          let sibling_offset = self.pager.write_page(Page::try_from(&sibling)?)?;
+          // siblings keys are larger than the split child, thus needs to be inserted
+          // at next index
+          children.insert(idx + 1, sibling_offset.clone());
+          keys.insert(idx, median.clone());
+
+          // write parent page to disk
+          self.pager.write_page_at_offset(Page::try_from(&*node)?, &node_offset)?;
+          // continue recursively
+          if kv.key <= median.0 {
+            self.insert_non_full(&mut child, child_offset, kv)
+          } else {
+            self.insert_non_full(&mut sibling, sibling_offset, kv)
+          }
+        } else {
+          self.insert_non_full(&mut child, child_offset, kv)
+        }
+      }
+      NodeType::Unexpected => Err(Error::UnexpectedError),
+    }
+  }
+
+  pub fn search(&mut self, key: String) -> Result<KeyValuePair, Error> {
+    let root_page = self.pager.get_page(&self.root_offset)?;
+    let root = Node::try_from(root_page)?;
+    self.search_node(root, &key)
+  }
+
+  fn search_node(&mut self, node: Node, search: &str) -> Result<KeyValuePair, Error> {
+    match node.node_type {
+      NodeType::Internal(children, keys) => {
+        let idx = keys.binary_search(&Key(search.to_string()))
+        .unwrap_or_else(|x| x);
+        // retrieve child page from disk and deserialize
+        let child_offset = children.get(idx).ok_or(Error::UnexpectedError)?;
+        let page = self.pager.get_page(child_offset)?;
+        let child_node = Node::try_from(page)?;
+        self.search_node(child_node, search)
+      }
+      NodeType::Leaf(pairs) => {
+        if let Ok(idx) = pairs.binary_search_by_key(&search.to_string(), |pair| pair.key.clone())
+        {
+          return Ok(pairs[idx].clone());
+        }
+        Err(Error::KeyNotFound)
+      }
+      NodeType::Unexpected => Err(Error::UnexpectedError),
+    }
+  }
+
+  /// delete deletes a given key from the tree
+  pub fn delete(&mut self, key: Key) -> Result<(), Error> {
+    self.delete_key_from_subtree(key, &self.root_offset.clone())
+  }
+
+  /// delete key from subtree recursively traverses a tree rooted at a node in certain offset
+  /// until it finds the given key and deletes
+  fn delete_key_from_subtree(&mut self, key: Key, offset: &Offset) -> Result<(), Error> {
+    let page = self.pager.get_page(offset)?;
+    let mut node = Node::try_from(page)?;
+
   }
 }
